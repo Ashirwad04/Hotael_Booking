e72234ab3bc01b7e0aa273787c39f420a2b44bbe
Ashirwad04 <ashirwadk04@gmail.com>
2024-06-03 16:07:02 +0530

    Adding Favorite in table it will mark tru or false

---
+++ src/main/java/AirBNB_Api/controller/FavoriteController.java
@@ -0,0 +1,60 @@
+package AirBNB_Api.controller;
+
+
+import AirBNB_Api.entity.AppUser;
+import AirBNB_Api.entity.Favorite;
+import AirBNB_Api.entity.Property;
+import AirBNB_Api.repo.FavoriteRepository;
+import AirBNB_Api.repo.PropertyRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/favorite")
+public class FavoriteController {
+
+    private FavoriteRepository fr;
+    private PropertyRepository pr;
+
+
+    public FavoriteController(FavoriteRepository fr, PropertyRepository pr) {
+        this.fr = fr;
+        this.pr = pr;
+    }
+
+
+    @PostMapping("/addFav")
+
+    public ResponseEntity<Favorite> addFavorite(
+            @AuthenticationPrincipal AppUser user,
+           @RequestParam long propertyId
+    ) {
+        Optional<Property> byId = pr.findById(propertyId);
+        Property property = byId.get();
+        Favorite favorite = new Favorite();
+        favorite.setAppUser(user);
+        favorite.setProperty(property);
+        favorite.setIsFavorite(true);
+
+        Favorite savedFavorite = fr.save(favorite);
+        return new ResponseEntity<>(savedFavorite, HttpStatus.CREATED);
+
+    }
+
+
+
+    @GetMapping("/userFavoriteList")
+    public ResponseEntity<List<Favorite>> getAllFavoritesOfUser(
+            @AuthenticationPrincipal AppUser user
+
+    ){
+        List<Favorite> favorites = fr.getFavorites(user);
+        return new ResponseEntity<>(favorites,HttpStatus.OK);
+
+    }
+}
---
+++ src/main/java/AirBNB_Api/entity/Favorite.java
@@ -0,0 +1,28 @@
+package AirBNB_Api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "favorite")
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "is_favorite", nullable = false)
+    private Boolean isFavorite = false;
+
+    @ManyToOne
+    @JoinColumn(name = "app_user_id")
+    private AppUser appUser;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+}
